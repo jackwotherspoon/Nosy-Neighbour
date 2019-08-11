3372ca98a862e2ae3d384e5684764c2415992646
Jack Wotherspoon <jackwotherspoon@hotmail.com>
2019-08-11 15:13:10 -0400

    Extracted meaningful data into pandas dataframe.

--- scraper.py
+++ scraper.py
@@ -1,9 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
-link = 'https://www.ottawarealestate.ca/search/details/no/4/' 
-response = requests.get(link, timeout = 5).text
-content = BeautifulSoup(response, "html.parser")
-textContent = content.findAll('div', attrs = {"class":"prop-descrip"})
-print(textContent)
-# testing commit user
+link = "https://www.ottawarealestate.ca/search/details/no/4/"
+page = requests.get(link, timeout = 5)
+content = BeautifulSoup(page.content, "html.parser")
+content.prettify()
+textContent = content.find_all('div', attrs = {"class":"prop-descrip"})
+filtered = []
+featureNames = [items.dt.get_text() for items in textContent]
+featureValues = [items.dd.get_text() for items in textContent]  
+data = pd.DataFrame({
+    "featureNames": featureNames,
+    "featureValues": featureValues
+})
+print(data)
