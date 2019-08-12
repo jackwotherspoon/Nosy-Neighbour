17dd517a5f6e8a0009a121c4f3e8818fe2a19c0e
Jack Wotherspoon <jackwotherspoon@hotmail.com>
2019-08-11 21:21:28 -0400

    Turn code into a function for efficiency.

--- scraper.py
+++ scraper.py
@@ -2,16 +2,17 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-link = "https://www.ottawarealestate.ca/search/details/no/4/"
-page = requests.get(link, timeout = 5)
-content = BeautifulSoup(page.content, "html.parser")
-content.prettify()
-textContent = content.find_all('div', attrs = {"class":"prop-descrip"})
-filtered = []
-featureNames = [items.dt.get_text() for items in textContent]
-featureValues = [items.dd.get_text() for items in textContent]  
-data = pd.DataFrame({
-    "featureNames": featureNames,
-    "featureValues": featureValues
-})
-print(data)
+def scrape(url, tag, classDescription):
+    page = requests.get(url, timeout = 5)
+    content = BeautifulSoup(page.content, "html.parser")
+    content.prettify()
+    textContent = content.find_all(tag, attrs = {"class": classDescription})
+    featureNames = [items.dt.get_text() for items in textContent]
+    featureValues = [items.dd.get_text() for items in textContent]  
+    data = pd.DataFrame({
+        "featureNames": featureNames,
+        "featureValues": featureValues
+    })
+    print(data)
+
+scrape("https://www.ottawarealestate.ca/search/details/no/4/", "div", "prop-descrip")
