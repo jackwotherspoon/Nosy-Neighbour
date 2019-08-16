d6c2d4ffbebfe8bb4ea2c691aa83a1505eece7ca
Jack Wotherspoon <jackwotherspoon@hotmail.com>
2019-08-15 22:02:48 -0400

    Adding to scraper function to continuously append to dataframe

--- scraper.py
+++ scraper.py
@@ -4,24 +4,26 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-def scrape(url, tag, classDescription):
+def scrape(url, tag, classDescription, dataset):
     page = requests.get(url, timeout = 5)
     if (page.status_code == 200):
         content = BeautifulSoup(page.content, "html.parser")
         content.prettify()
         textContent = content.find_all(tag, attrs = {"class": classDescription})
         featureNames = [items.dt.get_text() for items in textContent]
-        featureValues = [items.dd.get_text() for items in textContent]  
-        data = pd.DataFrame({
-            "featureNames": featureNames,
-            "featureValues": featureValues
-        })
-        print(data)
+        featureValues = [items.dd.get_text() for items in textContent]
+        dataset = dataset.append(pd.DataFrame([featureValues], columns = featureNames, index = [0]), ignore_index = True)
+        return dataset
     else:
         print("ERROR: Page request was unsuccessful.")
 
+# create dataframe and run function to grow dataframe
+data = pd.DataFrame([])
+
 # successful function call
-scrape("https://www.ottawarealestate.ca/search/details/no/4/", "div", "prop-descrip")
+data = scrape("https://www.ottawarealestate.ca/search/details/no/10/", "div", "prop-descrip", data)
+data = scrape("https://www.ottawarealestate.ca/search/details/no/9/", "div", "prop-descrip", data)
+print(data)
 
 # failure function call
-scrape("https://www.garbage.com/url", "div", "prop-descrip")
+scrape("https://www.garbage.com/url", "div", "prop-descrip", data)
