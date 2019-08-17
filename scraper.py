a3abd59753dff430e2f2c70315c629bec2c23bcc
Jack Wotherspoon <jackwotherspoon@hotmail.com>
2019-08-17 19:21:28 -0400

    Changed scrape function to return dict instead of dataframe.

--- scraper.py
+++ scraper.py
@@ -3,6 +3,7 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import numpy as np
 
 def scrape(url, tag, classDescription, dataset):
     page = requests.get(url, timeout = 5)
@@ -12,17 +13,15 @@ def scrape(url, tag, classDescription, dataset):
         textContent = content.find_all(tag, attrs = {"class": classDescription})
         featureNames = [items.dt.get_text() for items in textContent]
         featureValues = [items.dd.get_text() for items in textContent]
-        dataset = dataset.append(pd.DataFrame([featureValues], columns = featureNames, index = [0]), ignore_index = True)
+        dataset = dict(zip(featureNames, featureValues))
         return dataset
     else:
         print("ERROR: Page request was unsuccessful.")
 
-# create dataframe and run function to grow dataframe
-data = pd.DataFrame([])
-
+data = []
 # successful function call
-data = scrape("https://www.ottawarealestate.ca/search/details/no/10/", "div", "prop-descrip", data)
-data = scrape("https://www.ottawarealestate.ca/search/details/no/9/", "div", "prop-descrip", data)
+data.append(scrape("https://www.ottawarealestate.ca/search/details/no/10/", "div", "prop-descrip", data))
+data.append(scrape("https://www.ottawarealestate.ca/search/details/no/9/", "div", "prop-descrip", data))
 print(data)
 
 # failure function call
