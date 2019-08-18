c9ff4a0b067602e778e2ca2b73968091f0bec943
Jack Wotherspoon <jackwotherspoon@hotmail.com>
2019-08-17 21:24:03 -0400

    Created getFeatures function to return list of feature names used to describe houses.

--- scraper.py
+++ scraper.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import numpy as np
 
+# function to scrape web page for listing details and return dictionary
 def scrape(url, tag, classDescription):
     page = requests.get(url, timeout = 5)
     if (page.status_code == 200):
@@ -18,6 +19,7 @@ def scrape(url, tag, classDescription):
     else:
         print("ERROR: Page request was unsuccessful.")
 
+# function to iterate through web pages and extra house listing details
 def generateData(size):
     # initialize empty list to store data
     data = []
@@ -27,5 +29,18 @@ def generateData(size):
         data.append(scrape(link, "div", "prop-descrip"))
     return data
 
+# function to extract list of all feature names from all pages to be used as columns for dataset
+def getFeatures(data):
+    # since not all listings have uniform feature details we need to extract every feature name to compare missing data
+    allFeatures = []    
+    for i in range(0, len(data)):
+        features = [*data[i]]
+        for feature in features:
+            if feature not in allFeatures:
+                allFeatures.append(feature)
+    print(allFeatures)
+    return allFeatures
+
 # driver code
 data = generateData(5)
+featureNames = getFeatures(data)
