056d10f0151bcb23c125993622566021724b20a2
Jack Wotherspoon <jackwotherspoon@hotmail.com>
2019-08-21 19:11:56 -0400

    Remove unnecessary columns from dataframe.

--- cleanData.py
+++ cleanData.py
@@ -1,1 +1,12 @@
 # file that cleans and preprocesses data in order to be used effectively by the ML model
+import pandas as pd
+
+# function to clean data
+def clean(data):
+    # drop unneeded columns from dataframe
+    newData = data.drop(columns = ['MLS® #', 'Status', 'Subtype', 'Country', 'Community Features'], axis = 1)
+    return newData
+
+data = pd.read_csv('houseData.csv')
+data = clean(data)
+print(data)
