2253ce3eccf5bcb3747bd343e3bc3464986e4069
Jack Wotherspoon <jackwotherspoon@hotmail.com>
2019-09-12 09:49:39 -0400

    Create skeleton code for building model

--- buildModel.py
+++ buildModel.py
@@ -1,1 +1,16 @@
 # file that builds, trains, and tests the ML model
+
+import pandas as pd
+
+# function to split data into training and test sets
+def splitData(data):
+
+# function to train model on training set
+def train(trainingData):
+
+# function to test model on test set
+def test(testData):
+
+# driver code
+data = pd.read_csv('cleanedData.csv')
+print(data)
