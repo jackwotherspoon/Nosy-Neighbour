adb69f8fd25c7d52692babf242e32ec7c69d796a
Jack Wotherspoon <jackwotherspoon@hotmail.com>
2019-09-16 20:07:08 -0400

    Import sklearn to split training and test set

--- buildModel.py
+++ buildModel.py
@@ -1,10 +1,12 @@
 # file that builds, trains, and tests the ML model
 
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 # function to split data into training and test sets
 def splitData(data):
-
+    x_train, x_test, y_train, y_test = train_test_split(data.drop(columns = ['List Price']), data['List Price'], test_size = 0.2)
+    
 # function to train model on training set
 def train(trainingData):
 
@@ -14,3 +16,4 @@ def test(testData):
 # driver code
 data = pd.read_csv('cleanedData.csv')
 print(data)
+splitData(data)
