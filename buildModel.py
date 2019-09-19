641047cff33a20aa2da3e781b200fd93625eb3c7
Jack Wotherspoon <jackwotherspoon@hotmail.com>
2019-09-18 21:07:25 -0400

    Print shape of training and test data

--- buildModel.py
+++ buildModel.py
@@ -6,6 +6,8 @@ from sklearn.model_selection import train_test_split
 # function to split data into training and test sets
 def splitData(data):
     x_train, x_test, y_train, y_test = train_test_split(data.drop(columns = ['List Price']), data['List Price'], test_size = 0.2)
+    print("Training data shape has x values of %s and y values of shape %s" % (x_train.shape, y_train.shape))
+    print("Testing data shape has x values of %s and y values of shape %s" % (x_test.shape, y_test.shape))
     
 # function to train model on training set
 def train(trainingData):
