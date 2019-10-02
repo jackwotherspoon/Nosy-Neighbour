f38e75247e0cfef243ad58ed2d828893396cca93
Jack Wotherspoon <jackwotherspoon@hotmail.com>
2019-10-01 21:19:03 -0400

    Switched how I split data.

--- buildModel.py
+++ buildModel.py
@@ -1,22 +1,28 @@
 # file that builds, trains, and tests the ML model
 
 import pandas as pd
+from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
 # function to split data into training and test sets
 def splitData(data):
-    x_train, x_test, y_train, y_test = train_test_split(data.drop(columns = ['List Price']), data['List Price'], test_size = 0.2)
-    print("Training data shape has x values of %s and y values of shape %s" % (x_train.shape, y_train.shape))
-    print("Testing data shape has x values of %s and y values of shape %s" % (x_test.shape, y_test.shape))
+    trainData = data.sample(frac=0.8, random_state = 0)
+    testData  = data.drop(trainData.index)
+    print("Training data shape has x values of %s and y values of shape %s" % (trainData.shape))
+    print("Testing data shape has x values of %s and y values of shape %s" % (testData.shape))
     
-    return x_train, x_test, y_train, y_test
-# function to train model on training set
-def train(trainingData):
+    return trainData, testData
 
+# function to train model on training set
+def train(trainData):
+    trainLabels = trainData.pop('List Price')
+    
 # function to test model on test set
 def test(testData):
+    testLabels = testData.pop('List Price')
 
 # driver code
 data = pd.read_csv('cleanedData.csv')
 print(data)
-x_train, x_test, y_train, y_test = splitData(data)
+trainingData, testingData = splitData(data)
+train(trainingData)
